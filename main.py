415db27c20a5ae55d95934ffd9b82a8d82c7c4e2
Natã <datandarosa@gmail.com>
2025-08-13 20:01:18 -0300

    In this commit the basic files were added

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame as pg
+
+pg.init()
+
+print("The Game has Started")
+window = pg.display.set_mode(size=(1280, 920))
+
+while True:
+    #todo Check all Events
+
+    for event in pg.event.get(): 
+        
+        #* Close Game
+        if event.type == pg.QUIT:
+            pg.quit()
+            quit()
+
+
+
+
+
